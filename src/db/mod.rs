0664236c063abaeca70e6e0f817a18a23ca67ff7
TheWisePigeon <zozozozeph@gmail.com>
2023-01-05 22:27:57 +0000

    added environment variable processing

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,6 +1,10 @@
 use mongodb::options::ClientOptions;
 use mongodb::{Client, Database};
 use rocket::fairing::AdHoc;
+// use std::env;
+// use std::ffi::OsString;
+
+
 
 pub fn init() -> AdHoc{
     AdHoc::on_ignite("Connecting to mongoo", | rocket | async {
@@ -16,6 +20,17 @@ pub fn init() -> AdHoc{
 }
 
 pub async fn establish_connection() -> mongodb::error::Result<Database> {
+    // let option_mongo_uri = env::var_os("DB_URL");
+    // let db_url: String;
+    // match  option_mongo_uri {
+    //     Some(url)=>{
+    //         let mongo_url= &url.to_string_lossy();
+    //         db_url = mongo_url.to_string();
+    //     },
+    //     None =>{
+    //         db_url = "mongodb://localhost:27017".to_string();
+    //     }
+    // }
     let mongo_uri = "mongodb://mongo:5206QBEhvK4biGt6xlKb@containers-us-west-170.railway.app:5735/?retryWrites=true&w=majority&authSource=admin";
     let client_options = ClientOptions::parse(mongo_uri).await?;
     let mongo_client = Client::with_options(client_options)?;
