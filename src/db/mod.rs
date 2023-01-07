5de0b658adfbeac4dc463723f6f0ccbecf285581
TheWisePigeon <zozozozeph@gmail.com>
2023-01-07 15:21:20 +0000

    feat: added prod db_url

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -18,19 +18,19 @@ pub fn init() -> AdHoc{
 }
 
 pub async fn establish_connection() -> mongodb::error::Result<Database> {
-    let option_mongo_uri = env::var_os("DB_URL");
-    let db_url: String;
-    match  option_mongo_uri {
-        Some(url)=>{
-            let mongo_url= &url.to_string_lossy();
-            db_url = mongo_url.to_string();
-        },
-        None =>{
-            db_url = "mongodb://localhost:27017".to_string();
-        }
-    }
-    // let mongo_uri = "mongodb://mongo:5206QBEhvK4biGt6xlKb@containers-us-west-170.railway.app:5735/?retryWrites=true&w=majority&authSource=admin";
-    let client_options = ClientOptions::parse(db_url).await?;
+    // let option_mongo_uri = env::var_os("DB_URL");
+    // let db_url: String;
+    // match  option_mongo_uri {
+    //     Some(url)=>{
+    //         let mongo_url= &url.to_string_lossy();
+    //         db_url = mongo_url.to_string();
+    //     },
+    //     None =>{
+    //         db_url = "mongodb://localhost:27017".to_string();
+    //     }
+    // }
+    let mongo_uri = "mongodb://mongo:5206QBEhvK4biGt6xlKb@containers-us-west-170.railway.app:5735/?retryWrites=true&w=majority&authSource=admin";
+    let client_options = ClientOptions::parse(mongo_uri).await?;
     let mongo_client = Client::with_options(client_options)?;
     let database = mongo_client.database("pigeoncms");
     println!("Connected to db successfully");
