721bc3d846c2ffc794e439fcbe80dc9c11243c9c
TheWisePigeon <zozozozeph@gmail.com>
2023-01-12 16:37:09 +0000

    feat: added request guard for auth token

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,2 +1,3 @@
 pub mod user;
 pub mod container;
+pub mod auth_token;
--- src/routes/containers.rs
+++ src/routes/containers.rs
@@ -1,8 +1,8 @@
-use crate::models::container;
+use crate::models::{container::Container, auth_token::AuthToken};
 use rocket::{State};
 use mongodb::Database;
 
-#[post("/create", format="json")]
-fn create_container(  ){
-
+#[post("/create")]
+pub fn create( auth_token: AuthToken ) -> String {
+    auth_token.0.to_string()
 }
