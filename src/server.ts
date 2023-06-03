34fb8446900a4e37c60b74d7784eb97ef40efe99
TheWisePigeon <zozozozeph@gmail.com>
2023-06-03 10:50:51 +0000

    feat: add express project

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import cors from "cors"
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
