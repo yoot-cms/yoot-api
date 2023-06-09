af1992ed6a4c560c59c42d427032fd860f1f1f02
TheWisePigeon <zozozozeph@gmail.com>
2023-06-09 12:43:33 +0000

    updates

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from "express"
 import cors from "cors"
+import * as dotenv from "dotenv"
+dotenv.config()
+import sql from "./db"
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
+    const db_version = await sql` select version() `;
+    console.log(db_version)
     console.log(`App listening on port ${PORT}`)
 })
