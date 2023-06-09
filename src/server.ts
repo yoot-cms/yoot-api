18dfe432b03af9cc3bbaec33eee1c0cd2b56b41f
TheWisePigeon <zozozozeph@gmail.com>
2023-06-09 13:42:14 +0000

    feat: create account

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,16 @@ import cors from "cors"
 import * as dotenv from "dotenv"
 dotenv.config()
 import sql from "./db"
+import { create_account, get_all_users } from "./services/Authentication"
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000
+
+app.post("/auth/register", create_account)
+
+app.get("/users", get_all_users)
 
 app.listen(PORT, async() => {
     const db_version = await sql` select version() `;
