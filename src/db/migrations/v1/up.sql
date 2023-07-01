5c59d89c480e48d89de951dea353808648847cef
TheWisePigeon <zozozozeph@gmail.com>
2023-07-01 10:08:14 +0000

    refactor project structure

--- src/db/migrations/v1/up.sql
+++
@@ -1,5 +0,0 @@
-create table users (
-    id UUID default gen_random_uuid() PRIMARY KEY,
-    email text not null,
-    password text not null
-);
--- src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcrypt"
-const salt_rounds = 5
-
-export function hash_password(password: string){
-    return bcrypt.hashSync(password, salt_rounds)
-}
