d94dcddc1a17955f1eb08b6a1dd3ed96ceed4791
TheWisePigeon <zozozozeph@gmail.com>
2023-07-05 23:20:25 +0000

    ref: prepare delete entity route

--- src/routes/entities/index.ts
+++ src/routes/entities/index.ts
@@ -1,11 +1,10 @@
 import { Router } from "express"
 import { authenticate_request } from "../../middlewares/Auth"
-import { get_entities } from "../../services/Entities"
+import { get_entities, delete_entity } from "../../services/Entities"
 
 const entities_router = Router()
 
 entities_router.route("/entities").get( authenticate_request, get_entities )
-entities_router.route("/entities").post( authenticate_request, get_entities )
-entities_router.route("/entities").delete( authenticate_request, get_entities )
+entities_router.route("/entities/:name").delete( authenticate_request, delete_entity )
 
 export default entities_router
--- src/services/Entities.ts
+++ src/services/Entities.ts
@@ -14,3 +14,14 @@ export async function get_entities(req: Request<{}, {}, { key: ApiKey }>, res: R
         return res.status(500).send()
     }
 } 
+
+export async function delete_entity( req: Request<{ name: string }, {}, { key: ApiKey }>, res: Response ){
+    try {
+        const { name } = req.params
+        console.log(name)
+        return res.status(200).send()
+    } catch (err) {
+        console.log(`Error in delete entity`)
+        return res.status(500).send()
+    }
+}
