ea3b13883d3a0cf296cce111841715809399393f
TheWisePigeon <zozozozeph@gmail.com>
2023-07-05 18:13:40 +0000

    add dummy route

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express"
 import express from "express"
 import cors from "cors"
 import * as dotenv from "dotenv"
-import { verify_api_key } from "./utils";
 dotenv.config()
 import sql from "./db"
 const app = express()
@@ -11,45 +10,8 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 
-//Creer une fonction qui prendre en parametre un string 
-//et qui retourne False si le string est vide ""
-//si le string ne contient pas exactement un seul "."
-//retourne true et les deux parties du tableau resultant du split du string si les conditions sont reunies
-
-app.get("/entity", (req: Request, res: Response)=>{
-    try {
-        const api_key = req.headers["x-api-key"] as string ?? ""
-        const api_key_is_valid = verify_api_key(api_key)
-    } catch (err) {
-        console.log(err)
-        return res.status(500).send()
-    }
-})
-
-app.post("/entity", (req: Request, res: Response)=>{
-    try {
-        const api_key = req.headers["x-api-key"] as string ?? ""
-    } catch (err) {
-        console.log(err)
-        return res.status(500).send()
-    }
-})
-app.put("/entity", (req: Request, res: Response)=>{
-    try {
-        const api_key = req.headers["x-api-key"] as string ?? ""
-    } catch (err) {
-        console.log(err)
-        return res.status(500).send()
-    }
-})
-
-app.delete("/entity", (req: Request, res: Response)=>{
-    try {
-        const api_key = req.headers["x-api-key"] as string ?? ""
-    } catch (err) {
-        console.log(err)
-        return res.status(500).send()
-    }
+app.get("/", (_req: Request, res: Response)=>{
+    return res.status(200).send("Hello from YOOT API")
 })
 
 app.listen(PORT, async() => {
