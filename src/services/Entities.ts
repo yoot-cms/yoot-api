5160ca0d1499f8ae0230913cbfb46b582aff8473
TheWisePigeon <zozozozeph@gmail.com>
2023-07-06 17:10:39 +0000

    feat: create entity if key has permission

--- src/services/Entities.ts
+++ src/services/Entities.ts
@@ -19,6 +19,9 @@ export async function get_entities(req: Request<{}, {}, { key: ApiKey }>, res: R
 export async function create_entity( req: Request<{}, {}, { name:string, schema:Record<string, string>, key: ApiKey }>, res: Response ){
     try {
         const { key:{ permissions, project }, name, schema } = req.body
+        if(!name || !schema) return res.status(400).send({
+            message:"name and schema are required"
+        })
         const parsed_permissions = JSON.parse(permissions) as Permission
         if(!parsed_permissions.create_permission) return res.status(403).send({
             message:"Key does not have permission to create entities"
