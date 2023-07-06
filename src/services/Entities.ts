33cd38af9271322ed502702927ee673cc95f204f
jokappo <kokerprim@gmail.com>
2023-07-06 19:30:09 +0000

    delete and trash function

--- src/services/Entities.ts
+++ src/services/Entities.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { type } from "os";
 import sql from "../db";
 import { ApiKey, Permission, entity_data_is_valid } from "../utils";
+import { json } from "stream/consumers";
 
 export async function get_entities(req: Request<{}, {}, { key: ApiKey }>, res: Response) {
     try {
@@ -46,31 +47,29 @@ export async function create_entity( req: Request<{}, {}, { name:string, schema:
     }
 }
 
-export async function delete_entity( req: Request<{ name: string }, {}, { key: ApiKey }>, res: Response ){
+export async function delete_entity( req: Request<{ name: string }, {trash? : boolean}, { key: ApiKey }>, res: Response ){
     try {
         const { project } = req.body.key
         const { name } = req.params
-        const trashed = await sql `select trashed from entity where name = ${name}`
-        
-        if (trashed.length === 1) {
-        await sql `DELETE FROM entity
-        WHERE name = ${name} and project = ${project} `      
-        }        
-        return res.status(200).send()
+        const {trash} = req.query
+        const {permissions} = req.body.key
+        if (trash === 'true') {
+            await sql `UPDATE entity SET trashed = true WHERE name = ${name} and project = ${project}`
+            return res.status(200).send({
+                message : "Entity trashed successfuly"
+            })
+        }
+        const parsed_permissions = JSON.parse(permissions) as Permission
+        if (!parsed_permissions.delete_permission) return res.status(403).send({
+            message:"Key does not have permission to delete entities"
+        })
+        await sql`DELETE FROM entity
+        WHERE name = ${name} and project = ${project}`
+        return res.status(200).send({
+            message: "Deleted successfully",
+        })
     } catch (err) {
         console.log(`Error in delete entity ${err}`)
-        return res.status(500).send()
-    }
-}
-
-export async function trash_entity( req: Request<{ name: string }, {}, { key: ApiKey }>, res: Response ){
-    try {
-        const { project } = req.body.key
-        const { name } = req.params
-        await sql `UPDATE entity SET trashed = true WHERE name = ${name} and project = ${project}`
-        return res.status(200).send()
-    } catch (err) {
-        console.log(`Error in trash entity`)
         return res.status(500).send()
     }
 }
