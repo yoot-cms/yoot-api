378faa858dfe5092bc9a5a158ba347ca175b1b86
jokappo <kokerprim@gmail.com>
2023-07-06 18:07:32 +0000

    delete and trash entity

--- src/routes/entities/index.ts
+++ src/routes/entities/index.ts
@@ -1,10 +1,10 @@
 import { Router } from "express"
 import { authenticate_request } from "../../middlewares/Auth"
-import { get_entities, delete_entity } from "../../services/Entities"
+import { get_entities, delete_entity, trash_entity } from "../../services/Entities"
 
 const entities_router = Router()
 
 entities_router.route("/entities").get( authenticate_request, get_entities )
 entities_router.route("/entities/:name").delete( authenticate_request, delete_entity )
-
+entities_router.route("/entities/:name").put( authenticate_request, trash_entity )
 export default entities_router
--- src/services/Entities.ts
+++ src/services/Entities.ts
@@ -17,10 +17,29 @@ export async function get_entities(req: Request<{}, {}, { key: ApiKey }>, res: R
 
 export async function delete_entity( req: Request<{ name: string }, {}, { key: ApiKey }>, res: Response ){
     try {
+        const { project } = req.body.key
         const { name } = req.params
+        const trashed = await sql `select trashed from entity where name = ${name}`
+        
+        if (trashed.length === 1) {
+        await sql `DELETE FROM entity
+        WHERE name = ${name} and project = ${project} `      
+        }        
         return res.status(200).send()
     } catch (err) {
         console.log(`Error in delete entity`)
+        return res.status(500).send()
+    }
+}
+
+export async function trash_entity( req: Request<{ name: string }, {}, { key: ApiKey }>, res: Response ){
+    try {
+        const { project } = req.body.key
+        const { name } = req.params
+        await sql `UPDATE entity SET trashed = true WHERE name = ${name} and project = ${project}`
+        return res.status(200).send()
+    } catch (err) {
+        console.log(`Error in trash entity`)
         return res.status(500).send()
     }
 }
