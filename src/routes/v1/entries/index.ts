647a13e81bc4288a38e66472877966228bd4369d
TheWisePigeon <zozozozeph@gmail.com>
2023-07-13 19:13:28 +0000

    feat: get entries in entity

---
+++ src/routes/v1/entries/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { authenticate_request } from "../../../middlewares/Auth";
+import { get_entries, create_entry } from "../../../services/v1/Entries";
+
+const entries_v1 = Router()
+
+entries_v1.route("/v1/entities/:name/entries").get( authenticate_request, get_entries )
+
+export default entries_v1
