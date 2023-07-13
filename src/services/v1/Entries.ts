0eb1d232e7545736d12e606a9229b38eddf7919f
TheWisePigeon <zozozozeph@gmail.com>
2023-07-13 23:43:32 +0000

    feat: update entry with id

--- src/services/v1/Entries.ts
+++ src/services/v1/Entries.ts
@@ -110,18 +110,73 @@ export async function get_entries(req: Request<{ entity_name: string }, {}, { ke
 
 export async function update_entry(req: Request<{ entity_name: string, entry_id: string }, {}, { key: ApiKey, value: Record<string, string | number | boolean> }>, res: Response) {
     try {
-        const { key:{ permissions, project }, value} = req.body
+        const { key: { permissions, project }, value } = req.body
         const { entry_id, entity_name } = req.params
         const parsed_permissions = JSON.parse(permissions) as Permission
-        if(!parsed_permissions.write_permission) return res.status(403).send({
-            message:"Key does not have the permissions to edit entries"
+        if (!parsed_permissions.write_permission) return res.status(403).send({
+            message: "Key does not have the permissions to edit entries"
         })
-        if(!entity_name || !entity_name) return res.status(400).send({
-            message:generic_error_message
+        if (!entity_name || !entity_name) return res.status(400).send({
+            message: generic_error_message
         })
-        if(!value || typeof value!=="object") return res.status(400).send({
-            message:generic_error_message
+        if (!value || typeof value !== "object") return res.status(400).send({
+            message: generic_error_message
         })
+        const [entity] = await sql<{ id: string, project: string, schema: Record<string, "Text" | "Number" | "Boolean" | "Image"> }[]>`select * from entity where name=${entity_name} and project=${project}`
+        if (!entity) return res.status(404).send({
+            message: "Entity not found"
+        })
+        const [entry] = await sql<{ id: string, entity: string, value: Record<string, string | number | boolean> }[]>` select * from entry where id=${entry_id} and entity=${entity.id}`
+        if (!entry) return res.status(404).send({
+            message: "Entry not found"
+        })
+        const keys_match_schema = Object.keys(value).every(field => Object.keys(entity.schema).includes(field))
+        if (!keys_match_schema) return res.status(400).send({
+            message: "Invalid entry value. Some fields are not present in the entity's schema"
+        })
+        const fields = Object.entries(entity.schema)
+        let entry_value = entry.value
+        await Promise.all(
+            fields.map(async ([field_name, field_type]) => {
+                const data = value[field_name]
+                if (data) {
+                    if (field_type === "Text") {
+                        if (typeof data !== "string") return res.status(400).send({
+                            message: `Field ${field_name} must be of type string`
+                        })
+                        entry_value[field_name] = data
+                    }
+                    if (field_type === "Number") {
+                        if (typeof data !== "number") return res.status(400).send({
+                            message: `Field ${field_name} must be of type number`
+                        })
+                        entry_value[field_name] = data
+                    }
+                    if (field_type === "Boolean") {
+                        if (typeof data !== "boolean") return res.status(400).send({
+                            message: `Field ${field_name} must be of type boolean`
+                        })
+                        entry_value[field_name] = data
+                    }
+                    if (field_type === "Image") {
+                        if (typeof data !== "string") return res.status(400).send({
+                            message: `Field ${field_name} must be a base64 encoded string`
+                        })
+                        if (!is_base_64(data as string)) return res.status(400).send({
+                            message: `Field ${field_name} must be a base64 encoded string`
+                        })
+                        const media_id = crypto.randomUUID()
+                        const { url } = await imagekit.upload({
+                            file: data as string,
+                            fileName: media_id
+                        })
+                        entry_value[field_name] = url
+                    }
+                }
+            })
+        )
+        await sql` update entry set value=${sql.json(entry_value)} where id=${entry.id} `
+        return res.status(200).send()
     } catch (err) {
         console.log(`Error in update entry ${err}`)
         return res.status(500).send({
