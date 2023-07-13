a42e5a8865cc780891534fd1cac8951d8fae7f2a
TheWisePigeon <zozozozeph@gmail.com>
2023-07-13 23:44:50 +0000

    register update route for entries

--- src/routes/v1/entries/index.ts
+++ src/routes/v1/entries/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticate_request } from "../../../middlewares/Auth";
-import { get_entries, create_entry, delete_entry } from "../../../services/v1/Entries";
+import { get_entries, create_entry, delete_entry, update_entry } from "../../../services/v1/Entries";
 
 const entries_v1 = Router()
 
@@ -8,6 +8,8 @@ entries_v1.route("/v1/entities/:entity_name/entries").get( authenticate_request,
 
 entries_v1.route("/v1/entities/:entity_name/entries").post( authenticate_request, create_entry )
 
-entries_v1.route("/v1/entities/:entity_name/entries/:entry_id").delete( authenticate_request, delete_entry)
+entries_v1.route("/v1/entities/:entity_name/entries/:entry_id").delete( authenticate_request, delete_entry )
+
+entries_v1.route("/v1/entities/:entity_name/entries/:entry_id").patch( authenticate_request, update_entry )
 
 export default entries_v1
