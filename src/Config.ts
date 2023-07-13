df37d791a1022db65eeb817ae3bd28ec92f0ee62
TheWisePigeon <zozozozeph@gmail.com>
2023-07-13 20:38:16 +0000

    feat: delete entry in entity

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 export const API_KEY_PREFIX = process.env.API_KEY_PREFIX as string
 
-export const generic_error_message = "Bad request. See Yoot documentation for more details about how to create an entity"
+export const generic_error_message = "Bad request. See Yoot documentation for more details about how to interact with the API"
 
 export const generic_server_error_message = "Something went wrong. Please try again or contact support"
--- src/services/v1/Entries.ts
+++ src/services/v1/Entries.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
-import { generic_server_error_message } from "../../Config";
+import { generic_error_message, generic_server_error_message } from "../../Config";
 import sql from "../../db";
-import { ApiKey } from "../../utils";
+import { ApiKey, Permission } from "../../utils";
 
 export async function create_entry( req: Request, res: Response ){
     try {
@@ -18,6 +18,9 @@ export async function get_entries( req: Request<{ entity_name: string }, {}, { k
     try {
         const { project } = req.body.key
         const { entity_name } = req.params
+        if(!entity_name) return res.status(400).send({
+            message:generic_error_message
+        })
         const [entity] = await sql<{id:string}[]>`select id from entity where name=${entity_name} and project=${project}`
         if(!entity) return res.status(404).send({
             message:"Entity not found"
@@ -36,5 +39,27 @@ export async function get_entries( req: Request<{ entity_name: string }, {}, { k
 
 export async function update_entry(){}
 
-export async function delete_entry(){}
-
+export async function delete_entry( req: Request<{ entity_name:string, entry_id:string }, {}, { key: ApiKey }>, res: Response ){
+    try {
+        const { project, permissions } = req.body.key
+        const { entity_name, entry_id } = req.params
+        const parsed_permissions = JSON.parse(permissions) as Permission 
+        if(!parsed_permissions.delete_permission) return res.status(403).send({
+            message:"Key does not have permission to delete entries"
+        })
+        if(!entity_name || !entry_id) return res.status(400).send({
+            message:generic_error_message
+        })
+        const [entity] = await sql<{id:string}[]>`select id from entity where name=${entity_name} and project=${project}`
+        if(!entity) return res.status(404).send({
+            message:"Entity not found"
+        })
+        await sql`delete from entry where id=${entry_id} and entity=${entity.id}`
+        return res.status(200).send()
+    } catch (err) {
+        console.log(`Error in delete entry ${err}`)
+        return res.status(500).send({
+            message:generic_server_error_message
+        })
+    }
+}
